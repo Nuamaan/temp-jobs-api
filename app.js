41f13018838651b52cbb5eebdee20410a57cb7a7
Nuamaan Sheikh <nuamaaniqbal@gmail.com>
2023-12-31 16:54:11 -0500

    swagger docs added

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet')
 
+//Swagger 
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 
@@ -31,6 +36,10 @@ app.use(xss());
 app.use(express.json());
 // extra packages
 
+app.get('/', (req, res) => {
+  res.send('<h1>jobs API</h1><a href="api-docs">Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
